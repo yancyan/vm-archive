fcda6856c7b4030195f39cdcb096282580414d2d
zhuyx <zhuyx@startimes.com.cn>
2020-09-17 11:41:42 +0800

    ...

--- src/main/java/com/vm/base/App.java
+++ src/main/java/com/vm/base/App.java
@@ -1,14 +1,14 @@
 package com.vm.base;
 
-/**             byteNum    defaultValue
- * byte         1          0
- * short        2          0
- * int          4          0
- * long         8          0
- * float        4          0
- * double       8          0
- * char         2          '\u0000'(空格)
- * boolean
+/**             byteNum    defaultValue       valueRange
+ * byte         1          0                  [-128, 127]
+ * short        2          0                  [-32768, 32767]
+ * int          4          0                  [-2^31, 2^31-1]
+ * long         8          0                  [-2^63, 2^63-1]
+ * float        4          0                  [-3.4E38, 3.4E38]
+ * double       8          0                  [-1.8E308, 1.8E308]
+ * char         2          '\u0000'(空格)     [0, 65535]
+ * boolean                                    [false, true]
  */
 public class App {
 
