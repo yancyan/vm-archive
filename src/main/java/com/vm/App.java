b3dc5f9be8b51da63ba1e5ba6ef9df3c6be91973
zhuyx <zhuyx@startimes.com.cn>
2020-09-17 11:34:29 +0800

    type

--- src/main/java/com/vm/App.java
+++ src/main/java/com/vm/App.java
@@ -1,7 +1,16 @@
 package com.vm;
 
+
 public class App {
     public static void main(String[] args) {
+        float toFloat = Float.intBitsToFloat(0X80000000);
+        System.out.println(toFloat);
 
+
+        // float sn1 = 0.0f / 0.0f;
+        // float f1 = 0.0f;
+        // float sn2 = f1 / f1;
+        // System.out.println("sn1 hex = " + Integer.toHexString(Float.floatToRawIntBits(sn1)));
+        // System.out.println("sn2 hex = " + Integer.toHexString(Float.floatToRawIntBits(sn2)));
     }
 }
---
+++ src/main/java/com/vm/base/App.java
@@ -0,0 +1,19 @@
+package com.vm.base;
+
+/**             byteNum    defaultValue
+ * byte         1          0
+ * short        2          0
+ * int          4          0
+ * long         8          0
+ * float        4          0
+ * double       8          0
+ * char         2          '\u0000'(空格)
+ * boolean
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+
+    }
+}
